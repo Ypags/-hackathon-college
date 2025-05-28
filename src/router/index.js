bdae74ed7e700f6b95a673a0b5ca5288508f2e55
Ypags <oartem.28082007@gmail.com>
2025-05-28 22:56:04 +0700

    добавлены новые страницы: Home и Equations. Обновлен роутер для использования новых страниц.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import EquationsView from '../views/EquationsView.vue'
+import HomeView from '../pages/Home.vue'
+import EquationsView from '../pages/Equations.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
